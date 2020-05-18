1cab5f7d6f35654c2f4218bbc2e3a0ec4e95ae7c
Brian <brian.yimtam@student.manchester.ac.uk>
2020-05-18 04:29:43 +0100

    formatting

--- commands/createchannel/index.js
+++ commands/createchannel/index.js
@@ -3,14 +3,23 @@ module.exports = {
   description: "Creates channel based on name",
   execute(msg, tokens) {
     var channelname = tokens[0];
-    if (tokens[1] != "earth" || tokens[1] != "mars" || tokens[1] != "galaxy" || tokens[1] != "fantasy" || tokens[1] != "families")
-    {
-        msg.reply("This world does not exist in our records. Perhaps you could fuck off to it?")
-        return;
+    if (
+      tokens[1] != "earth" ||
+      tokens[1] != "mars" ||
+      tokens[1] != "galaxy" ||
+      tokens[1] != "fantasy" ||
+      tokens[1] != "families"
+    ) {
+      msg.reply(
+        "This world does not exist in our records. Perhaps you could fuck off to it?"
+      );
+      return;
     }
     var world = msg.guild.channels.resolve(tokens[1]);
     var server = msg.guild;
-    var category = server.channels.cache.find((channel) => channel.name === tokens[1]);
+    var category = server.channels.cache.find(
+      (channel) => channel.name === tokens[1]
+    );
 
     if (
       msg.member.roles.cache.find((role) => role.name === "Admin") ||
@@ -28,11 +37,9 @@ module.exports = {
       });
       msg.reply(`The channel ${channelname} has been created.`);
       return;
+    } else {
+      msg.reply(`You have no power here.`);
+      return;
     }
-    else
-    {
-        msg.reply(`You have no power here.`);
-        return;
-    }
-  }
+  },
 };
