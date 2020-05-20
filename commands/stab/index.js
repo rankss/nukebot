13d6bc9f84dfcc741536bc2cdd3c994000662fbb
RankSS <benyang4869@gmail.com>
2020-05-19 18:13:28 -0700

    changed spacing

--- commands/stab/index.js
+++ commands/stab/index.js
@@ -1,29 +1,29 @@
 module.exports = {
-  name: "stab",
-  description: "STAB",
-  execute(msg, tokens) {
-    const memes = [
-      "https://images7.memedroid.com/images/UPLOADED199/564b9147dbe18.jpeg",
-      "https://i.kym-cdn.com/photos/images/original/001/235/567/bfd.jpg",
-      "https://i.kym-cdn.com/entries/icons/original/000/027/328/character.jpg",
-    ];
-    const chosenmeme = [memes[Math.floor(Math.random() * memes.length)]];
-    console.log(chosenmeme);
-    var mention = tokens[0];
-    if (!mention) return;
+    name: "stab",
+    description: "STAB",
+    execute(msg, tokens) {
+        const memes = [
+            "https://images7.memedroid.com/images/UPLOADED199/564b9147dbe18.jpeg",
+            "https://i.kym-cdn.com/photos/images/original/001/235/567/bfd.jpg",
+            "https://i.kym-cdn.com/entries/icons/original/000/027/328/character.jpg",
+        ];
+        const chosenmeme = [memes[Math.floor(Math.random() * memes.length)]];
+        console.log(chosenmeme);
+        var mention = tokens[0];
+        if (!mention) return;
 
-    if (mention.startsWith("<@") && mention.endsWith(">")) {
-      mention = mention.slice(2, -1);
+        if (mention.startsWith("<@") && mention.endsWith(">")) {
+            mention = mention.slice(2, -1);
 
-      if (mention.startsWith("!")) {
-        mention = mention.slice(1);
-      }
-      console.log(mention);
-      mention = "<@" + mention + ">";
-      msg.channel.send(tokens[0], {
-        mention,
-        files: chosenmeme,
-      });
-    }
-  },
+            if (mention.startsWith("!")) {
+                mention = mention.slice(1);
+            }
+            console.log(mention);
+            mention = "<@" + mention + ">";
+            msg.channel.send(tokens[0], {
+                mention,
+                files: chosenmeme,
+            });
+        }
+    },
 };
